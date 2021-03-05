a2823b0913517ba249f86772176dcb7d0506aff4
Clarence Jones <clarence.jones@codepoetllc.com>
2021-03-05 10:22:09 -0600

    End of section 4

---
+++ API/DTOs/LoginDto.cs
@@ -0,0 +1,14 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace API.DTOs
+{
+    public class LoginDto
+    {
+        
+        
+        public string UserName {get; set;}
+        
+        
+        public string Password {get; set;}
+    }
+}
